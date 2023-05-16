73dd4395851931ee2aa2ecd9088a227cc7246268
kafreen13 <afreen.khan@synectiks.com>
2023-05-16 14:58:47 +0530

    removed cost data in client file

--- client/client.go
+++ client/client.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/Appkube-awsx/awsx-stepFunctions/awssession"
-	"github.com/aws/aws-sdk-go/service/costexplorer"
 	"github.com/aws/aws-sdk-go/service/sfn"
 
 	"log"
@@ -19,10 +18,6 @@ func GetClient(region string, crossAccountRoleArn string, accessKey string, secr
 	return assumeRole(crossAccountRoleArn, sessionName, externalId, accessKey, secretKey, region)
 }
 
-func GetCostClient(region string, crossAccountRoleArn string, accessKey string, secretKey string, externalId string) *costexplorer.CostExplorer {
-	sessionName := "assume_role_session_name"
-	return assumeRoleForCost(crossAccountRoleArn, sessionName, externalId, accessKey, secretKey, region)
-}
 
 func assumeRole(roleArn string, sessionName string, externalId string, accesskey string, secretKey string, region string) *sfn.SFN {
 	sess, err := awssession.GetSessionByCreds(region, accesskey, secretKey, "")
@@ -63,45 +58,4 @@ func assumeRole(roleArn string, sessionName string, externalId string, accesskey
 	sfnClient := sfn.New(awsSession)
 
 	return sfnClient
-}
-
-func assumeRoleForCost(roleArn string, sessionName string, externalId string, accesskey string, secretKey string, region string) *costexplorer.CostExplorer {
-	sess, err := awssession.GetSessionByCreds(region, accesskey, secretKey, "")
-
-	if err != nil {
-		fmt.Printf("failed to create aws session, %v\n", err)
-		log.Fatal(err)
-	}
-
-	svc := sts.New(sess)
-
-	assumeRoleInput := sts.AssumeRoleInput{
-		RoleArn:         aws.String(roleArn),
-		RoleSessionName: aws.String(sessionName),
-		DurationSeconds: aws.Int64(60 * 60 * 1),
-	}
-
-	if externalId != "nil" {
-		fmt.Println("Trying to fetch external id to assume new role")
-		assumeRoleInput.ExternalId = aws.String(externalId)
-	}
-
-	result, err := svc.AssumeRole(&assumeRoleInput)
-
-	if err != nil {
-		fmt.Printf("failed to assume role, %v\n", err)
-		log.Fatal(err)
-	}
-	//fmt.Println("Assume role output: ", result)
-
-	awsSession, err := awssession.GetSessionByCreds("us-east-1", *result.Credentials.AccessKeyId, *result.Credentials.SecretAccessKey, *result.Credentials.SessionToken)
-
-	if err != nil {
-		fmt.Printf("failed to assume role, %v\n", err)
-		log.Fatal(err)
-	}
-
-	costClient := costexplorer.New(awsSession)
-
-	return costClient
 }
